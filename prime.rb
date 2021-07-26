61d3ac619c90023a738c370fed1b4b0dcbd395f8
Ambar Khare <ambar.khare24@gmail.com>
2021-07-26 10:06:22 -0700

    commit tree b6eb4f98557edbbeecb0b8eaba3907c1db34adfd

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+# Method below tells if a certain number is prime or not.  Prime numbers are only divisible by itself and 1.
+
+def prime?(number)
+    if number > 1
+        (2..number-1).to_a.none? {|num| number % num == 0} #if the # is prime, then none of the elements will meet the condition and the return value will be true.
+    else
+        false
+    end
+end
